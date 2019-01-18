15c3389882429563a862e582b0f4f7ede60e3c40
Pedro4000 <Vetpreislen1>
2019-01-18 11:53:28 +0100

    third commit

--- src/MainBundle/Controller/DefaultController.php
+++ src/MainBundle/Controller/DefaultController.php
@@ -10,8 +10,11 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session;
 
 use MainBundle\Entity\users;
+use MainBundle\Entity\favourite;
+use MainBundle\Entity\comments;
 
 
 class DefaultController extends Controller
@@ -57,7 +60,9 @@ class DefaultController extends Controller
                 $em->persist ($user);
                 $em->flush();
 
-                return $this->redirectToRoute('main_homepage', array('id'=>$user->getId()));
+                $request->getSession()->getFlashBag()->add('notice', 'Your account has successfully been created');
+
+                return $this->redirectToRoute('main_homepage');
 
             }
 
@@ -66,8 +71,69 @@ class DefaultController extends Controller
             return $this->render('MainBundle:Default:register.html.twig', array('form'=>$form->createView()));
 
 
+    }
+
+
+        public function loginAction(Request $request)
+    {
+
+        $user= new users;
+        $formBuilder =$this -> get('form.factory')->createBuilder(Formtype::class,$user);
+
+        $formBuilder
+            ->add('email', TextType::class)
+            ->add('password', TextType::class)
+            ->add('submit',      SubmitType::class);
+
+        $form= $formBuilder->getForm();
+
+        if ($request->isMethod('POST')) 
+        {
+            $form->handleRequest($request);
+
+
+            if ($form->isValid())
+            {
+                $usermail=$user->getEmail();
+                $userpassword=$user->getPassword();
+
+                var_dump($usermail,$userpassword);
+                $em = $this->getDoctrine()
+                  ->getManager();
+                $repo=$em->getRepository('MainBundle:users');
+
+                $user=$repo->findOneBy(array('email'=>$usermail));
+
+                var_dump($user);
+
+                return $this->render('MainBundle:Default:login.html.twig', array(
+                    'form'=>$form->createView(),
+                    'user'=>$user));
+
+                if (isset($user)) {
+                    var_dump('un utilisateur à été trouvé');
+                }
+
+            
+
             }
+        
+        }
+        return $this->render('MainBundle:Default:login.html.twig', array(
+            'form'=>$form->createView()));
 
 
+    }
+
+            public function logoutAction()
+    {
+        return $this->render('MainBundle:Default:logout.html.twig');
+    }
+
+
+            public function favouriteAction()
+    {
+        return $this->render('MainBundle:Default:favourite.html.twig');
+    }
 
 }
--- src/MainBundle/Entity/users.php
+++ src/MainBundle/Entity/users.php
@@ -45,7 +45,7 @@ class users
     /**
      * @var string
      *
-     * @ORM\Column(name="email", type="string", length=255, unique=true)
+     * @ORM\Column(name="email", type="string", length=255, unique=false)
      */
     private $email;
 
